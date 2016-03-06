78b14c3f30bfa40ccc88fbdb9a08530c1a9baf61
vermouth1992 <czhangseu@gmail.com>
2016-03-06 13:21:38 -0800

    finish project0

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,23 +4,54 @@
 
 public class NBody {
 
+    static String imageRoot = "./images/";
+    static String background = "starfield.jpg";
+
     public static void main (String[] args) {
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
         double radius = readRadius(filename);
+        // planet array
         Planet[] pArray = readPlanets(filename);
-        String background = "./images/starfield.jpg";
 
         StdDraw.setScale(-radius, radius);
         StdDraw.clear();
-        StdDraw.picture(0, 0, background);
+        drawBackgroundPlanet(pArray);
 
+        // animation loop
+        double t = 0;
+        double[] xForces = new double[pArray.length];
+        double[] yForces = new double[pArray.length];
+
+        while (t < T) {
+            // calculate xForces and yForces
+            for (int i = 0; i < pArray.length; i++) {
+                xForces[i] = pArray[i].calcNetForceExertedByX(pArray);
+                yForces[i] = pArray[i].calcNetForceExertedByY(pArray);
+            }
+            // update planet's position
+            for (int i = 0; i < pArray.length; i++) {
+                pArray[i].update(dt, xForces[i], yForces[i]);
+            }
+            drawBackgroundPlanet(pArray);
+            StdDraw.show(10);
+            t += dt;
+        }
+
+        StdOut.printf("%d\n", pArray.length);
+        StdOut.printf("%.2e\n", radius);
+        for (int i = 0; i < pArray.length; i++) {
+            StdOut.printf("%11.4e %11.4e %11.4e %11.4e %11.4e %12s\n",
+                    pArray[i].xxPos, pArray[i].yyPos, pArray[i].xxVel, pArray[i].yyVel, pArray[i].mass, pArray[i].imgFileName);
+        }
+    }
+
+    public static void drawBackgroundPlanet(Planet[] pArray) {
+        StdDraw.picture(0, 0, imageRoot + background);
         for (Planet p: pArray) {
             p.draw();
         }
-
-        StdDraw.show(2000);
     }
 
     public static double readRadius (String filename) {
@@ -34,7 +65,7 @@ public class NBody {
         int numPlanets = in.readInt();
         double radius = in.readDouble();
         Planet[] pArray = new Planet[numPlanets];
-        String imageRoot = "./images/";
+
         for (int i = 0; i < numPlanets; i++) {
             pArray[i] = new Planet(0.0, 0.0, 0.0, 0.0, 0.0, "");
             pArray[i].xxPos = in.readDouble();
